69d721de429a8c87d090095bbcc0d932efc4eb4a
Pradeep_Ranasinghe <pradeep@climatesi.com>
2023-07-13 13:46:10 +0530

    latest chnage

---
+++ src/calculation/jica-railway-passenger/dto/project.dto.ts
@@ -0,0 +1,13 @@
+export class ProjectDto {
+
+    year:number;
+    // btkm: number;
+    p:number; // : Increased number of passenger by the project activity in year y (passenger/y)
+    btdp:number; // Average trip distance of the passenger transportation in the project in year y (km)
+    ec: number; // : Electricity consumption associated with the operation of the project activity in year y (MWh/y)
+    ef: number;
+
+  fc:number; //Consumption of fuel i associated with the operation of the railway in year y (t/y)
+    ncv: number;
+    efpkm:number;
+}
---
+++ src/json-file-viwer/json-file-viwer.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
+import { JsonFileViwerService } from './json-file-viwer.service';
+
+@Controller('filename')
+@UseGuards(LocalAuthGuard)
+export class JsonFileViwerController {
+
+    constructor(public service: JsonFileViwerService,
+        
+    ) {
+    }
+
+    @Get(':name')
+    public methodology(@Param('name') name) {
+ console.log(name)
+        return this.service.selectJson(name);
+    }
+}
