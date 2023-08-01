9714029143d87ecbde63245036d3edff7322457f
Camila Haas Primieri <camilahp@unops.org>
2023-08-01 14:26:58 +0200

    Set up CI/CD deployment

--- src/calculation/unfccc-am0016-v-5/dto/electricity.dto.ts
+++ src/calculation/unfccc-am0016-v-5/dto/electricity.dto.ts
@@ -1,32 +1,17 @@
 import { sourceDto } from "./source.dto";
 
 export class electrictyDto{
-
-
     telix:number;
-   
     efco2x:number;
     secix: number;
-   
-
     pelix:number;
     delix:number;
-
     ecblky:number;
     efelky:number;
     tdlky:number;
-
-
     ecpjjy:number;
-
-    secjny:number;//a
-    ddjny:number;//a
+    secjny:number;
+    ddjny:number;
     efefjy:number;
     tdljy:number;
-
-
-    
-    
-    
-
 }
